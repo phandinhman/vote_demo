8fe99bf3ab3e88088940d27398369e11aaf72b2c
ptnk1995 <nguyenphuoctiendn@gmail.com>
2017-08-13 12:34:03 +0700

    add theme

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,6 @@ class User < ApplicationRecord
   class << self
     def from_omniauth auth
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        binding.pry
         user.provider = auth.provider
         user.uid = auth.uid
         user.email = auth.info.email
